f3c43dd721028c66c9caf885c3d91e0f1f25829f
thinny <tony.sutin1234@gmail.com>
2018-02-18 00:09:38 +0700

    add upload audio to firebase storage
    add upload audio to firebase storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,24 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { AudioRecordPage } from '../pages/audio-record/audio-record';
 import { IonicStorageModule } from '@ionic/storage';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { IonicAudioModule, WebAudioProvider, CordovaMediaProvider, defaultAudioProviderFactory } from 'ionic-audio';
+export function myCustomAudioProviderFactory() {
+  return (window.hasOwnProperty('cordova')) ? new CordovaMediaProvider() : new WebAudioProvider();
+}
+
+export const firebase_config = {
+    apiKey: "AIzaSyBXh1LjxE22AwPj2zgMp1jip_Q1U0MkKgk",
+    authDomain: "ion3-7bb36.firebaseapp.com",
+    databaseURL: "https://ion3-7bb36.firebaseio.com",
+    projectId: "ion3-7bb36",
+    storageBucket: "ion3-7bb36.appspot.com",
+    messagingSenderId: "1033574228295"
+};
 
 @NgModule({
   declarations: [
@@ -18,7 +36,12 @@ import { IonicStorageModule } from '@ionic/storage';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    IonicStorageModule.forRoot()
+    IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp(firebase_config),
+     AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    IonicAudioModule.forRoot(defaultAudioProviderFactory),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/audio-record/audio-record.ts
+++ src/pages/audio-record/audio-record.ts
@@ -1,10 +1,21 @@
+import { Item } from './audio-record';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { NavController, NavParams, Platform, DateTime } from 'ionic-angular';
 import { Media, MediaObject } from '@ionic-native/media';
 import { File } from '@ionic-native/file';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase';
+import { AudioProvider } from 'ionic-audio';
 
-
-@IonicPage()
+export interface Item {
+  audioName?: string,
+  audioPath?: string,
+  audioDuration?: number,
+  audioCreate?: string,
+  audioUpload?: string,
+  audioUploadUrl?: string
+}
 @Component({
   selector: 'page-audio-record',
   templateUrl: 'audio-record.html',
@@ -19,9 +30,17 @@ export class AudioRecordPage {
   interval: any;
   duration: number = 0;
 
+  audioItems: Observable<Item[]>;
+  audioCollections: AngularFirestoreCollection<Item>;
+
   constructor(public navCtrl: NavController,
               public navParams: NavParams, private media: Media,
-              private file: File, public platform: Platform) {
+              private file: File, public platform: Platform,
+              private _firestore: AngularFirestore, private _audioProvider: AudioProvider) {
+
+    this.audioCollections = _firestore.collection<Item>('voice_collections');
+    this.audioItems = this.audioCollections.valueChanges();
+
   }
 
   ionViewDidLoad() {
@@ -30,33 +49,53 @@ export class AudioRecordPage {
   }
   startRecord(){
     if (this.platform.is('ios')) {
-    this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.wav';
-    this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + this.fileName;
-    this.audio = this.media.create(this.filePath);
-  } else if (this.platform.is('android')) {
-    this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.wav';
-    this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + this.fileName;
-    this.audio = this.media.create(this.filePath);
-  }
-  this.audio.startRecord();
-  this.recording = true;
-  this.getDuration();
+      this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.wav';
+      this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + this.fileName;
+      this.audio = this.media.create(this.filePath);
+    } else if (this.platform.is('android')) {
+        this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.wav';
+        this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + this.fileName;
+        this.audio = this.media.create(this.filePath);
+    }
+
+    this.audio.startRecord();
+    this.recording = true;
+    this.getDuration();
   }
   getAudioList() {
-    if(localStorage.getItem("audiolist")) {
-      this.audioList = JSON.parse(localStorage.getItem("audiolist"));
-      console.log(JSON.stringify(this.audioList));
-    }
+    //this.audioItems.subscribe(audioData => console.log('audioItems: ', JSON.stringify(audioData)))
+    this.audioItems = this.audioCollections.valueChanges();
+
   }
   stopRecord() {
     this.audio.stopRecord();
-    let data = { filename: this.fileName, duration: this.duration };
-    this.audioList.push(data);
-    localStorage.setItem("audiolist", JSON.stringify(this.audioList));
     this.recording = false;
+
+    let audioData: Item = {
+      audioName: this.fileName,
+      audioDuration: this.duration,
+      audioPath: this.filePath,
+      audioUpload: 'me',
+      audioCreate: new Date().toString(),
+      audioUploadUrl: null
+    }
+
     clearInterval(this.interval);
     this.duration = 0;
     this.getAudioList();
+
+
+    this.startUploadAudio(audioData.audioName).then(fileURL => {
+      console.log('upload files OK')
+      audioData.audioUploadUrl = fileURL;
+      this.addFirebase(audioData);
+
+      this.removeAudioFile(this.file.externalDataDirectory, audioData.audioName).then(fileRemove => {
+        console.log('remove files OK')
+      })
+
+    });
+
   }
   getDuration(){
     this.interval = setInterval(() => {
@@ -67,23 +106,33 @@ export class AudioRecordPage {
       console.log(this.duration)
    }, 1000);
   }
-playAudio(file,idx) {
-    if (this.platform.is('ios')) {
-      this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + file;
-      this.audio = this.media.create(this.filePath);
-    } else if (this.platform.is('android')) {
-      this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + file;
-      this.audio = this.media.create(this.filePath);
-    }
-    this.audio.play();
-    this.audio.setVolume(0.8);
+  playAudio(audioFile: Item) {
   }
-  removeFile(filename, index){
-    localStorage.removeItem('audiolist');
-    this.getAudioList();
+
+  removeAudioFile(path, fileName) {
+    return this.file.removeFile(path, fileName);
   }
-  clearRecord(){
-    localStorage.clear();
-    this.getAudioList();
+
+  addFirebase(item: Item){
+    this.audioCollections.add(item).then(data => console.log(data)).catch(err => console.log(err));
+    console.log('addItem OK')
+  }
+  startUploadAudio(fileName: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      let storageRef = firebase.storage().ref();
+      this.file.readAsDataURL(this.file.externalDataDirectory, fileName).then((file) => {
+        let voiceRef = storageRef.child('voices/'+fileName).putString(file, firebase.storage.StringFormat.DATA_URL);
+        voiceRef.on(firebase.storage.TaskEvent.STATE_CHANGED, (snapshot) => {
+          console.log("uploading");
+        }, (e) => {
+          reject(e)
+          console.log(JSON.stringify(e, null, 2));
+        }, () => {
+          var downloadURL = voiceRef.snapshot.downloadURL;
+          resolve(downloadURL);
+        });
+      });
+    });
+
   }
 }
